0582c7593eed2515133cb5926887c4840c03a189
Henrique Santos <thenrique395@hotmail.com>
2018-09-22 19:43:33 -0300

    Criação de rotas

--- src/app/funcionario/funcionario.component.ts
+++ src/app/funcionario/funcionario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'qualit-funcionario',
   templateUrl: './funcionario.component.html',
   styleUrls: ['./funcionario.component.css']
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'qualit-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-   title = 'Qualificão';
+   title = 'Qualificação';
   constructor() { }
 
   ngOnInit() {
