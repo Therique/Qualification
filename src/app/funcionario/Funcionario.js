fe20003d3b3ae8eb4fb738e9198af289975b1c87
Henrique Santos <thenrique395@hotmail.com>
2018-09-23 21:05:39 -0300

    Criação das qualificações

---
+++ src/app/funcionario/Funcionario.js
@@ -0,0 +1,17 @@
+
+
+$(function () {
+    var estrelas = $('.star');
+    var escolhida = 0;
+
+    function repaint(e) {
+        var indice = $(this).index() + 1;
+        if (e.type == 'click') escolhida = indice;
+        estrelas.removeClass('full');
+        var upTo = $(this).hasClass('star-wrapper') ? escolhida : indice;
+        estrelas.slice(0, upTo).addClass('full');
+    }
+
+    $('.star-wrapper').on('mouseleave', repaint);
+    estrelas.on('mouseenter mouseleave click', repaint);
+});
--- src/app/funcionario/funcionario.component.ts
+++ src/app/funcionario/funcionario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'qualit-funcionario',
@@ -10,8 +11,11 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbModalConfig, NgbModal]
 })
 export class FuncionarioComponent implements OnInit {
-
-  liguagens = ['c#', 'JAVA'];
+  liguagens = ['Angular', 'Java', 'C', 'C++', 'Python', 'C#', 'JavaScript', 'Visual Basic .NET', 'R',
+    'PHP', 'MATLAB', 'Swift', 'Objective-C', 'Assembly', 'Perl', 'Ruby', 'Delphi / Object Pascal', 'Go', 'Scratch',
+    'PL/SQL',
+    'Visual Basic'
+  ];
 
   constructor(config: NgbModalConfig, private modalService: NgbModal) {
     // customize default values of modals used by this component tree
